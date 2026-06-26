b0087e15fa6a2da0145909b877b33fbcd74729c9
Harkirat <harkirat96@gmail.com>
2026-06-26 21:49:12 +0530

    init

---
+++ backend/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { createClient } from "redis";
+
+const client = createClient();
+client.connect();
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/submission", (req, res) => {
+    const userId = req.body.userId;
+    const questionId = req.body.questionId;
+    const code = req.body.code;
+    const language = req.body.language;
+    // put this entry in the DB
+    client.lPush("problems", JSON.stringify({userId, questionId, code, language}))
+
+    res.json({
+        message: "processing",
+        
+    })
+})
+
+app.get("/submission/:submissionId", (req, res) => {
+    
+})
+
+
+app.listen(3000);
