6e09674b68b4e7dbefd7a428c6069c36daf7e60e
Harkirat <harkirat96@gmail.com>
2026-06-28 21:45:38 +0530

    Checkpoint v2

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { createClient } from "redis";
+import { prisma } from "./db";
+import cors from "cors";
 
 const client = createClient();
 client.connect();
@@ -7,23 +9,38 @@ client.connect();
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
-app.post("/submission", (req, res) => {
-    const userId = req.body.userId;
-    const questionId = req.body.questionId;
+app.post("/submission", async (req, res) => {
     const code = req.body.code;
     const language = req.body.language;
+
+   const response = await prisma.submissions.create({
+        data: {
+            language,
+            code,
+            status: "Processing"
+        }
+    })
     // put this entry in the DB
-    client.lPush("problems", JSON.stringify({userId, questionId, code, language}))
+    client.lPush("problems", JSON.stringify({submissionId: response.id, code, language}))
 
     res.json({
         message: "processing",
-        
+        id: response.id
     })
 })
 
-app.get("/submission/:submissionId", (req, res) => {
-    
+app.get("/submission/:submissionId", async (req, res) => {
+    const response = await prisma.submissions.findFirst({
+        where: {
+            id: req.params.submissionId
+        }
+    })
+
+    res.json({
+        submission: response
+    })
 })
 
 
